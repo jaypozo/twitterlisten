d54f0fa89c06c338265913b19fa6b35a037ee9f5
Jay Pozo <jay@jaymatter.com>
2013-05-26 18:41:19 -0700

    Implementing OAuth 1.0

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 var OAuth = require('OAuth')
-,   stream_endpoint = 'https://stream.twitter.com/1.1/statuses/filter.json'
-,   req_token_endpoint = 'https://api.twitter.com/oauth/request_token'
-,   access_token_endpoint = 'https://api.twitter.com/oauth/access_token'
 ,   auth_data = require('./auth_data.json')
 ,   consumer_key = auth_data.consumer_key
-,   appl_secret = auth_data.appl_secret
-,   parameters = '?track='
-,   watch_words = ['#yolo','kelowna'];
+,   consumer_secret = auth_data.consumer_secret
+,   access_token = auth_data.access_token
+,   access_token_secret = auth_data.access_token_secret;
 
+var oauth = new OAuth.OAuth(
+  'https://api.twitter.com/oauth/request_token'
+  , 'https://api.twitter.com/oauth/access_token'
+  , consumer_key
+  , consumer_secret
+  , '1.0A'
+  , null
+  , 'HMAC-SHA1'
+);
 
-var req = https.request(https_options, function(res){
-  console.log("statusCode: ", res.statusCode);
-  console.log("headers: ", res.headers);
-
-  res.on('data',function(data){
-    process.stdout.write(data);
-  });
-});
-req.end();
-
-req.on('error', function(err){
-  console.error(err);
-})
+oauth.get(
+  'https://stream.twitter.com/1.1/statuses/filter.json?track=kelowna'
+  , access_token
+  , access_token_secret,
+  function(err,data,res){
+    if (err) throw err;
+    console.log(require('util').inspect(data));
+  }
+)
