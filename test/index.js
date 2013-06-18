6e952f67b859a0fd5b400e807129ae7048135662
Jay Pozo <jay@jaymatter.com>
2013-06-17 22:19:53 -0700

    Added test for new module format

---
+++ test/index.js
@@ -0,0 +1,14 @@
+var TwitterListen = require('../index.js');
+
+
+var twitter_listen = new TwitterListen(["polo","yolo"]);
+
+twitter_listen.on('tweet_error', function(err){
+  console.log('error');
+  console.log(err);
+})
+
+twitter_listen.on('tweet', function(tweet){
+  console.log(tweet.text);
+})
+
